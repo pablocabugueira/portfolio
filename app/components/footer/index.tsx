90c0ca4b144af7fc0000cf8138efa8b49c30f54a
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-07-10 22:10:08 -0300

    create footer

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,17 @@
+import { IoMdHeart } from "react-icons/io"
+
+export const Footer = () => {
+    return (
+        <footer className="h-14 w-full flex items-center justify-center bg-gray-950">
+            <span className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400">
+                Made
+                <IoMdHeart
+                    size={13}
+                    className="text-emerald-500"
+                />
+                by
+                <strong className="font-medium">Pablo Galvão Cabugueira</strong>
+            </span>
+        </footer>
+    )
+}
