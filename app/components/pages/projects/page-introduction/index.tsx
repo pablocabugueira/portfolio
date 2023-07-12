82320bcd5578244e49d01b1050b69fc4a44594f2
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-07-12 11:42:43 -0300

    create page introduction of projects page

---
+++ app/components/pages/projects/page-introduction/index.tsx
@@ -0,0 +1,25 @@
+import { HiArrowNarrowLeft } from "react-icons/hi"
+import { CustomLink } from "../../../link"
+import { SectionTitle } from "../../../section-title"
+
+export const PageIntroduction = () => {
+    return (
+        <section className="w-full h-[450px] lg:h-[630px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center px-2">
+            <SectionTitle
+                subtitle="projetos"
+                title="Meus projetos"
+                className="text-center items-center [&>h3]:text-4xl"
+            />
+
+            <div className="flex flex-col items-center">
+                <p className="text-gray-400 text-center max-w-[640px] my-6 text-sm sm:text-base">
+                    Aqui você poderá ver alguns dos trabalhos que eu desenvolvi. Navegue à vontade e explore os projetos para ver como foram criados, as tecnologias utilizadas e as funcionalidades implementadas.
+                </p>
+                <CustomLink href="/">
+                    <HiArrowNarrowLeft size={20} />
+                    Voltar para Home
+                </CustomLink>
+            </div>
+        </section>
+    )
+}
