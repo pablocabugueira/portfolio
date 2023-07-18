046cfa1fbc9896f7a5450489e0ce12a44a05af0e
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-07-17 23:47:57 -0300

    create back to top button

---
+++ app/components/back-to-top/index.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { TbArrowNarrowUp } from "react-icons/tb"
+import { CustomButton } from "../button"
+import { useCallback, useEffect, useState } from "react"
+
+export const BackToTop = () => {
+    const [show, setShow] = useState(false)
+
+    const scrollToTop = () => {
+        window.scrollTo({ top:0, behavior: "smooth"})
+    }
+
+    const handleScroll = useCallback(() => {
+        if(!show && window.scrollY > 500) setShow(true)
+        if(show && window.scrollY <= 500) setShow(false)
+    }, [show])
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+      }, [handleScroll])
+
+    return (
+        <div className="fixed right-4 bottom-4 z-20">
+            {show ? (
+                <CustomButton onClick={scrollToTop} className="shadow-lg shadow-emerald-400/20">
+                    <TbArrowNarrowUp size={20} />
+                </CustomButton>
+            ): null}
+            
+        </div>
+    )
+}
