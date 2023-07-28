9a5994a098bccd8aeda36592e1e7b4d3b43185c6
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-07-27 21:46:37 -0300

    Project Details using Hygraph

--- app/components/pages/projects/projects-list/project-card/index.tsx
+++ app/components/pages/projects/projects-list/project-card/index.tsx
@@ -1,6 +1,13 @@
+import { Project } from "@/app/types/projects"
 import Image from "next/image"
 
-export const ProjectCard = () => {
+type ProjectCardProps = {
+    project: Project
+}
+
+export const ProjectCard = ({ project }: ProjectCardProps) => {
+    const technologies = project.technologies.map(x => x.name).join(", ")
+
     return (
         <div className="rounded-lg h-[436px] flex flex-col bg-gray-800 overflow-hidden border-2 border-gray-800 hover:border-emerald-500 opacity-70 hover:opacity-100
         transition-all group">
@@ -8,19 +15,23 @@ export const ProjectCard = () => {
                 <Image
                     width={380}
                     height={200}
-                    src="/images/preview.png"
-                    alt="Thumbnail do projeto BitCent"
+                    src={project.thumbnail.url}
+                    alt={`Thumbnail do projeto ${project.title}`}
                     unoptimized
                     className="w-full h-full object-cover group-hover:scale-110 duration-500 transition-all"
                 />
             </div>
 
             <div className="flex-1 flex flex-col p-8">
-                <strong className="font-medium text-gray-50/90 group-hover:text-emerald-500 transition-all">Bitcent</strong>
+                <strong className="font-medium text-gray-50/90 group-hover:text-emerald-500 transition-all">
+                    {project.title}
+                </strong>
                 <p className="mt-2 text-gray-400 line-clamp-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, sed labore facilis velit repudiandae ad repellendus atque commodi, debitis eveniet perspiciatis maiores in quidem ipsum.
+                    {project.shortDescription}
                 </p>
-                <span className="text-gray-300 text-sm font-medium block mt-auto truncate">Next.js, TypeScript, Git, Github, JavaScript, Html, Css</span>
+                <span className="text-gray-300 text-sm font-medium block mt-auto truncate">
+                    {technologies}
+                </span>
             </div>
         </div>
     )
