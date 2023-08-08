9356a3b2f4d9af6c73d9eee28fe6c1baa5b757fc
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-08-08 00:23:22 -0300

    back to top button animation (framer motion)

--- app/components/back-to-top/index.tsx
+++ app/components/back-to-top/index.tsx
@@ -3,6 +3,7 @@
 import { TbArrowNarrowUp } from "react-icons/tb"
 import { CustomButton } from "../button"
 import { useCallback, useEffect, useState } from "react"
+import { AnimatePresence, motion } from "framer-motion"
 
 export const BackToTop = () => {
     const [show, setShow] = useState(false)
@@ -22,13 +23,19 @@ export const BackToTop = () => {
       }, [handleScroll])
 
     return (
-        <div className="fixed right-4 bottom-4 z-20">
-            {show ? (
-                <CustomButton onClick={scrollToTop} className="shadow-lg shadow-emerald-400/20">
-                    <TbArrowNarrowUp size={20} />
-                </CustomButton>
-            ): null}
-            
-        </div>
+        <AnimatePresence>
+            {show && (
+                <motion.div
+                    className="fixed right-4 bottom-4 z-20"
+                    initial={{ opacity: 0, right: -10 }}
+                    animate={{ opacity: 1, right: 16 }}
+                    exit={{ opacity: 0, right: -10 }}
+                >
+                    <CustomButton onClick={scrollToTop} className="shadow-lg shadow-emerald-400/20">
+                        <TbArrowNarrowUp size={20} />
+                    </CustomButton>
+                </motion.div>
+            )}
+        </AnimatePresence>
     )
 }
